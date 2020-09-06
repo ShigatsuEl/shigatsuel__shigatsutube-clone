e5870d45b205d77ee747e40f8d2fac545360e6ed
MinChan Lee <shigatsu970704@gmail.com>
2020-09-06 22:04:00 +0900

    7.Express Core Routing

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
 import express from "express";
-import logger from "morgan";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import { userRouter } from "./router";
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: https://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from my ass!!");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser());
-app.use(bodyParser.json({}));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(morgan("dev"));
@@ -26,4 +21,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter);
+
+export default app;
