13c56d9b775ea9f69fcbe169b20b34650af45a80
MinChan Lee <shigatsu970704@gmail.com>
2020-09-08 16:41:36 +0900

    20.Home Controller Part Two

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ export const videos = [
     title: "Video Awesome",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Lee Minchan",
@@ -16,7 +17,8 @@ export const videos = [
     title: "Video Super",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Lee Minchan",
@@ -28,7 +30,8 @@ export const videos = [
     title: "Video Nice",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Lee Minchan",
@@ -40,7 +43,8 @@ export const videos = [
     title: "Video Perfect",
     description: "This is something I love",
     views: 24,
-    videoFile: "https://archive.org/details/BigBuckBunny_124",
+    videoFile:
+      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
     creator: {
       id: 121212,
       name: "Lee Minchan",
