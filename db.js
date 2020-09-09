e23904c51e2a59af2019d18a53c82710279d571a
MinChan Lee <shigatsu970704@gmail.com>
2020-09-09 17:32:55 +0900

    24.Connecting To MongoDB

--- db.js
+++ db.js
@@ -1,54 +1,17 @@
-export const videos = [
-  {
-    id: 324393,
-    title: "Video Awesome",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Lee Minchan",
-      email: "minchan@gmail.com",
-    },
-  },
-  {
-    id: 1212121,
-    title: "Video Super",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Lee Minchan",
-      email: "minchan@gmail.com",
-    },
-  },
-  {
-    id: 55555,
-    title: "Video Nice",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Lee Minchan",
-      email: "minchan@gmail.com",
-    },
-  },
-  {
-    id: 11111,
-    title: "Video Perfect",
-    description: "This is something I love",
-    views: 24,
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "Lee Minchan",
-      email: "minchan@gmail.com",
-    },
-  },
-];
+import mongoose from "mongoose";
+
+mongoose.connect("mongodb://localhost:27017/ShigatsuTube", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅  Conneted to DB");
+
+const handleError = (error) =>
+  console.log(`❌  Error On DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
