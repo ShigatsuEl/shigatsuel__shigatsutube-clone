bb35d48b1e9e8461511435dd8f0add89ddb8d91f
MinChan Lee <shigatsu970704@gmail.com>
2020-09-16 13:09:03 +0900

    52.Github Log In Part One

--- passport.js
+++ passport.js
@@ -1,5 +1,16 @@
 import passport from "passport";
+import GithubStrategy from "passport-github";
+import { githubLoginCallback } from "./controllers/userControllers";
 import User from "./models/User";
+
+passport.use(
+  new GithubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: "http://localhost:4000/auth/github/callback",
+  }),
+  githubLoginCallback
+);
 
 passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
