ad1c8e218452d83b379fbd5dcfc99f5d17c49992
MinChan Lee <shigatsu970704@gmail.com>
2020-09-23 17:29:22 +0900

    66.Mute Unmute and Fixed Play Pause Functionallity

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,17 +1,39 @@
 const videoContainer = document.getElementById("jsVideoPlayer");
-let videoPlayer = document.querySelector("#jsVideoPlayer video");
+const videoPlayer = document.querySelector("#jsVideoPlayer video");
 const playBtn = document.getElementById("jsPlayButton");
+const volumeBtn = document.getElementById("jsVolumeButton");
+
+function handleVolumeClick() {
+  if (videoPlayer.muted) {
+    videoPlayer.muted = false;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+  } else {
+    videoPlayer.muted = true;
+    volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+  }
+}
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
     videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
   } else {
     videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
   }
 }
 
+function handleVideoPlayer() {
+  if (videoPlayer.played) {
+    playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+  } else {
+    playBtn.innerHTML = '<i class="fas fa-play"></i>';
+  }
+}
 function init() {
+  videoPlayer.addEventListener("play", handleVideoPlayer);
   playBtn.addEventListener("click", handlePlayClick);
+  volumeBtn.addEventListener("click", handleVolumeClick);
 }
 
 if (videoContainer) {
