b30f874e19e395ad17e825612e9c8f4f005dba06
MinChan Lee <shigatsu970704@gmail.com>
2020-10-15 10:34:54 +0900

    120.Reply Comment Part Thirteen(Update Reply Edit and Complete)

--- assets/js/editReplyComment.js
+++ assets/js/editReplyComment.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
-const replyContainer = document.getElementById("jsReplyContainer");
+const commentContainer = document.getElementById("jsCommentContainer");
 const replySubinfo = document.querySelector(".reply__subinfo");
 const replyText = document.querySelectorAll(".reply__text");
 
 let replyId;
+let replyContainer;
 let replyBlock;
 let replyContent;
 let replyEditBox;
@@ -13,10 +14,21 @@ let replyEditInput;
 let replyEditCancelBtn;
 let replyEditSaveBtn;
 
+// Edit버튼을 누르고 ReplyInput(답글입력)을 Enter & Shift + Enter 입력 시 발생하는 이벤트!
+
 const handleEnter = async (event) => {
   if (window.event.keyCode === 13) {
     if (!window.event.shiftKey) {
       event.preventDefault();
+      replyId =
+        event.target.parentElement.parentElement.parentElement.parentElement
+          .dataset.id;
+      console.log(replyId);
+      replyBlock = document.getElementById(`${replyId}`);
+      replyContent = replyBlock.querySelector("#jsReplyContent");
+      replyEditBox = replyBlock.querySelector("#jsReplyEditBox");
+      replyEditInput = replyBlock.querySelector("#jsReplyEditInput");
+
       const newReply = replyEditInput.innerHTML;
       const response = await axios({
         url: `/api/${replyId}/edit-reply`,
@@ -34,35 +46,55 @@ const handleEnter = async (event) => {
     }
   }
 };
+// Edit버튼을 누르고 Save 버튼을 클릭할 때 발생하는 이벤트!
 
 const handleSave = async (event) => {
-  event.preventDefault();
-  const newReply = replyEditInput.innerHTML;
-  const response = await axios({
-    url: `/api/${replyId}/edit-reply`,
-    method: "post",
-    data: {
-      editReply: newReply,
-    },
-  });
-  if (response.status === 200) {
-    replyContent.innerHTML = newReply;
-    replyEditInput.innerHTML = newReply;
-    replyContent.classList.toggle("hidden");
-    replyEditBox.classList.toggle("hidden");
+  if (event.target.className.includes("editSaveBtn")) {
+    event.preventDefault();
+    replyId =
+      event.target.parentElement.parentElement.parentElement.parentElement
+        .parentElement.dataset.id;
+    replyBlock = document.getElementById(`${replyId}`);
+    replyContent = replyBlock.querySelector("#jsReplyContent");
+    replyEditBox = replyBlock.querySelector("#jsReplyEditBox");
+    replyEditInput = replyBlock.querySelector("#jsReplyEditInput");
+
+    const newReply = replyEditInput.innerHTML;
+    const response = await axios({
+      url: `/api/${replyId}/edit-reply`,
+      method: "post",
+      data: {
+        editReply: newReply,
+      },
+    });
+    if (response.status === 200) {
+      replyContent.innerHTML = newReply;
+      replyEditInput.innerHTML = newReply;
+      replyContent.classList.toggle("hidden");
+      replyEditBox.classList.toggle("hidden");
+    }
   }
 };
 
+// Edit버튼을 누르고 Cancel 버튼을 클릭할 때 발생하는 이벤트!
+
 const handleCancle = (event) => {
-  event.preventDefault();
-  replyContent.classList.toggle("hidden");
-  replyEditBox.classList.toggle("hidden");
+  if (event.target.className.includes("editCancelBtn")) {
+    event.preventDefault();
+    replyId =
+      event.target.parentElement.parentElement.parentElement.parentElement
+        .parentElement.dataset.id;
+    replyBlock = document.getElementById(`${replyId}`);
+    replyContent = replyBlock.querySelector("#jsReplyContent");
+    replyEditBox = replyBlock.querySelector("#jsReplyEditBox");
+    replyEditBox.classList.toggle("hidden");
+    replyContent.classList.toggle("hidden");
+  }
 };
 
 const handleEdit = (event) => {
-  /* console.log(event.target.parentElement.parentElement.parentElement.parentElement.dataset.id); */
-
-  if (event.target.className.includes("editReplyBtn")) {
+  if (event.target.className.includes("replyEditBtn")) {
+    /* console.log(event.target.parentElement.parentElement.parentElement.parentElement.dataset.id); */
     //assign variables
     replyId =
       event.target.parentElement.parentElement.parentElement.parentElement
@@ -77,13 +109,15 @@ const handleEdit = (event) => {
 
     replyContent.classList.toggle("hidden");
     replyEditBox.classList.toggle("hidden");
+
     const reply = replyContent.innerHTML;
     replyEditInput.innerHTML = reply;
     replyEditInput.focus();
-    replyEditCancelBtn.addEventListener("click", handleCancle);
-    replyEditForm.addEventListener("submit", handleSave);
+
+    // replyEditForm.addEventListener("submit", handleSave);
     replyEditInput.addEventListener("keydown", handleEnter);
     replyEditSaveBtn.addEventListener("click", handleSave);
+    replyEditCancelBtn.addEventListener("click", handleCancle);
   }
 };
 
@@ -92,7 +126,7 @@ function init() {
   for (let i = 0; i < replyText.length; i++) {
     replyText[i].innerHTML = replyText[i].innerText;
   }
-  replyContainer.addEventListener("click", handleEdit);
+  commentContainer.addEventListener("click", handleEdit);
 }
 
 if (replySubinfo) {
